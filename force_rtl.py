ad8ba953abe5da1841c7310d9cab22d5adfae1f5
esmaeelE <esmaeelEE@protonmail.com>
2025-10-04 12:14:03 +0330

    Add files via upload

---
+++ force_rtl.py
@@ -0,0 +1,17 @@
+# force to RTL
+
+
+# read a file line by line add char
+
+
+char_to_add = "‮"
+char_to_add.encode("utf8")
+
+# Open the file in read mode
+with open("input.txt", "r", encoding="utf-8") as file:
+    # Read lines and add the character to the start of each line
+    modified_lines = [char_to_add + line for line in file]
+
+    # Optionally, write the modified lines to a new file
+with open("output.txt", "w", encoding="utf-8") as file:
+    file.writelines(modified_lines)
